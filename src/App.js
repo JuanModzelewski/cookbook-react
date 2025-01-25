2d4bc1e949ad55ab5901fc0851ee542cb53fc999
JuanModzelewski <juanmodzelewski@gmail.com>
2025-01-25 17:28:05 +0100

    Sign up added to pages / auth, Axios defaults added and form validation attached

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
 import { Container } from 'react-bootstrap';
 import styles from '../src/App.module.css';
+import './api/axiosDefaults';
 import NavBar from './components/NavBar';
+import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
   return (
     <div className={styles.App}>
-      <NavBar />
-      <Container className={styles.Main}>
+      <Container>
+        <NavBar />
       </Container>
+        <Routes>
+          <Route path="/signup" element={<SignUpForm />} />
+        </Routes>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,17 +11,17 @@ const NavBar = () => {
   return (
     <Navbar expand="md" className={styles.Navbar} fixed="top">
       <Container>
-        <Navbar.Brand to="/"><img src={Logo} alt="Logo" height={90} /></Navbar.Brand>
+        <Navbar.Brand href="/"><img src={Logo} alt="Logo" height={90} /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
           <Nav>
-          <Nav.Link to="/recipe/create" className={`${styles.NavLink}`}><i className="fa-regular fa-square-plus"></i>Add Recipe</Nav.Link>
-            <Nav.Link to="/browse" className={styles.NavLink}><i className="fa-solid fa-book"></i>Browse</Nav.Link>
-            <Nav.Link to="/favorites" className={styles.NavLink}><i className="fa-regular fa-heart"></i>Favorites</Nav.Link>
-            <Nav.Link to="signOut" className={styles.NavLink}><i className="fa-solid fa-right-from-bracket"></i>Sign Out</Nav.Link>
-            <Nav.Link to="/signIn" className={styles.NavLink}><i className="fa-solid fa-right-to-bracket"></i>Sign In</Nav.Link>
-            <Nav.Link to="/signup" className={styles.NavLink}><i className="fa-solid fa-user-plus"></i>Sign Up</Nav.Link>
-            <Nav.Link to="/profile" className={styles.NavLink}><Avatar src={''} height={45} width={45} />Profile</Nav.Link>
+          <Nav.Link href="/recipe/create" className={`${styles.NavLink}`}><i className="fa-regular fa-square-plus"></i>Add Recipe</Nav.Link>
+            <Nav.Link href="/browse" className={styles.NavLink}><i className="fa-solid fa-book"></i>Browse</Nav.Link>
+            <Nav.Link href="/favorites" className={styles.NavLink}><i className="fa-regular fa-heart"></i>Favorites</Nav.Link>
+            <Nav.Link href="signOut" className={styles.NavLink}><i className="fa-solid fa-right-from-bracket"></i>Sign Out</Nav.Link>
+            <Nav.Link href="/signIn" className={styles.NavLink}><i className="fa-solid fa-right-to-bracket"></i>Sign In</Nav.Link>
+            <Nav.Link href="/signup" className={styles.NavLink}><i className="fa-solid fa-user-plus"></i>Sign Up</Nav.Link>
+            <Nav.Link href="/profile" className={styles.NavLink}><Avatar src={''} height={45} width={45} />Profile</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
