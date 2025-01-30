0c10400bdeaa64f7c0bfea592f5f661950e40ba5
JuanModzelewski <juanmodzelewski@gmail.com>
2025-01-31 00:14:36 +0100

    Removed token to test on mobile

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -31,8 +31,6 @@ const SignInForm = () => {
         event.preventDefault()
         try {
             const { data } = await axios.post("/dj-rest-auth/login/", signInData)
-            const token = `${data.access}`;
-            localStorage.setItem("refreshTokenTimestamp", token);
             setCurrentUser(data.user)
             navigate("/")
         } catch (error) {
